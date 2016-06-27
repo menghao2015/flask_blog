6891757b858e3680530026cdad0534f0d96317f4
root <root@localhost.localdomain>
2016-06-27 10:29:26 +0800

    empty_index

--- manage.py
+++ manage.py
@@ -15,5 +15,13 @@ def make_shell_context():
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
+
+@manager.command
+def deploy():
+	"""Run deployment tasks."""
+	# 创建数据库表
+	
+	db.create_all()
+
 if __name__ == '__main__':
 	manager.run()
